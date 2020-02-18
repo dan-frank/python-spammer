cae77d89d259e35ea88c91473adf97ffe7822440
dan-frank <dan.frank.lucas@gmail.com>
2020-02-18 18:41:07 +0000

    Updated scam to run with python3

--- scam.py
+++ scam.py
@@ -7,19 +7,19 @@ import json
 chars = string.ascii_letters + string.digits + '!@#$%^&*()'
 random.seed = (os.urandom(1024))
 
-url = 'POST_URL'
+url = 'http://craigslist.pottsfam.com/index872dijasydu2iuad27aysdu2yytaus6d2ajsdhasdasd2.php'
 
 names = json.loads(open('names.json').read())
 
 for name in names:
-    name_extra = ''.join(random.choice(string.digits))
+	name_extra = ''.join(random.choice(string.digits))
 
-    username = name.lower() + name_extra + '@yahoo.com'
-    password = ''.join(random.choice(chars) for i in range(12))
+	username = name.lower() + name_extra + '@yahoo.com'
+	password = ''.join(random.choice(chars) for i in range(8))
 
-    requests.post(url, allow_redirects=False, data={
-        'username': username,
-        'password': password
-    })
+	requests.post(url, allow_redirects=False, data={
+		'auid2yjauysd2uasdasdasd': username,
+		'kjauysd6sAJSDhyui2yasd': password
+	})
 
-    print 'sending username %s and password %s' % (username, password)
+	print ("sending username {un} and password {pw}".format(un=username, pw=password))
