14ca6b397f8948afc07646e3fa4a8d4ce4635b50
dan-frank <dan.frank.lucas@gmail.com>
2020-02-18 19:04:40 +0000

    Read lastnames

--- scam.py
+++ scam.py
@@ -10,11 +10,12 @@ random.seed = (os.urandom(1024))
 url = 'http://craigslist.pottsfam.com/index872dijasydu2iuad27aysdu2yytaus6d2ajsdhasdasd2.php'
 
 names = json.loads(open('names.json').read())
+lastnames = json.loads(open('lastnames.json').read())
 
 for name in names:
 	name_extra = ''.join(random.choice(string.digits))
 
-	username = name.lower() + name_extra + '@yahoo.com'
+	username = name.lower() + lastnames[random.randint(0,1000)].lower() + name_extra + '@yahoo.com'
 	password = ''.join(random.choice(chars) for i in range(8))
 
 	requests.post(url, allow_redirects=False, data={
